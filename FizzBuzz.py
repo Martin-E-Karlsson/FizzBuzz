1d62f991312946c907bfde599bed8083f187c00b
Martin Karlsson <m.karlsson91@gmail.com>
2020-09-14 13:37:04 +0200

    Changed x variable to message

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,16 +1,16 @@
 def main():
     for i in range(1, 101):
-        x = ""
+        message = ""
         if i % 3 == 0 and (i == 42) is False:
-            x = x + "Fizz"
+            message = message + "Fizz"
         if i % 5 == 0:
-            x = x + "Buzz"
-        if x == "":
-            x = i
+            message = message + "Buzz"
+        if message == "":
+            message = i
         if i == 42:
             print("Answer to the Ultimate Question of Life, the Universe, and Everything")
         elif (i == 42) is False:
-            print(x)
+            print(message)
 
 
 if __name__ == "__main__":
