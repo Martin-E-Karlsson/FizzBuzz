a6f1d023cb96de32eb01cd8ddb0cc5486b568ca4
Martin Karlsson <m.karlsson91@gmail.com>
2020-09-14 13:59:08 +0200

    Added Joakim Wassbergs solution as fizzbuzz2

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,25 +1,35 @@
 def main():
-    fizzbuzz1(1, 100)
+    # fizzbuzz1(1, 100)
+    fizzbuzz2(1, 100)
 
 
 def fizzbuzz1(lower_limit, upper_limit):
     for i in range(lower_limit, upper_limit + 1):
-        message = ""
+        x = ""
         if i % 3 == 0 and (i == 42) is False:
-            message = message + "Fizz"
+            x = x + "Fizz"
         if i % 5 == 0:
-            message = message + "Buzz"
-        if message == "":
-            message = i
+            x = x + "Buzz"
+        if x == "":
+            x = i
         if i == 42:
             print("Answer to the Ultimate Question of Life, the Universe, and Everything")
         elif (i == 42) is False:
-            print(message)
+            print(x)
 
 
 def fizzbuzz2(lower_limit, upper_limit):
-    for i in range(lower_limit, upper_limit):
-        pass
+    for number in range(lower_limit, upper_limit + 1):
+        if number == 42:
+            print("Answer to the Ultimate Question of Life, the Universe, and Everything", end="")
+        else:
+            if number % 3 == 0:
+                print("Fizz", end="")
+            if number % 5 == 0:
+                print("Buzz", end="")
+            if number % 3 != 0 and number % 5 != 0:
+                print(number, end="")
+        print()
 
 
 if __name__ == "__main__":
