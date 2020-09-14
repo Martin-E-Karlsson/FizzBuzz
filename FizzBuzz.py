27c3f06435f18ec319f45032296149a8bfda27ad
Martin Karlsson <m.karlsson91@gmail.com>
2020-09-14 13:30:57 +0200

    Put code inside main

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,18 +1,17 @@
 def main():
-    pass
+    for i in range(1, 101):
+        x = ""
+        if i % 3 == 0 and (i == 42) is False:
+            x = x + "Fizz"
+        if i % 5 == 0:
+            x = x + "Buzz"
+        if x == "":
+            x = i
+        if i == 42:
+            print("Answer to the Ultimate Question of Life, the Universe, and Everything")
+        elif (i == 42) is False:
+            print (x)
 
-for i in range(1,101):
-    x = ""
-    if i % 3 == 0 and (i == 42) is False:
-        x = x + "Fizz"
-    if i % 5 == 0:
-        x = x + "Buzz"
-    if x == "":
-        x = i
-    if i == 42:
-        print("Answer to the Ultimate Question of Life, the Universe, and Everything")
-    elif (i == 42) is False:
-        print (x)
-        
+
 if __name__ == "__main__":
     main()
