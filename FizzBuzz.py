877ff1c8383fd6f674624c4693824341411674c1
Martin Karlsson <m.karlsson91@gmail.com>
2020-09-14 13:45:23 +0200

    Split program into several functions to contain several different ways solving FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,5 +1,9 @@
 def main():
-    for i in range(1, 101):
+    fizzbuzz1(1, 100)
+
+
+def fizzbuzz1(lower_limit, upper_limit):
+    for i in range(lower_limit, upper_limit + 1):
         message = ""
         if i % 3 == 0 and (i == 42) is False:
             message = message + "Fizz"
@@ -11,6 +15,11 @@ def main():
             print("Answer to the Ultimate Question of Life, the Universe, and Everything")
         elif (i == 42) is False:
             print(message)
+
+
+def fizzbuzz2(lower_limit, upper_limit):
+    for i in range(lower_limit, upper_limit):
+        pass
 
 
 if __name__ == "__main__":
