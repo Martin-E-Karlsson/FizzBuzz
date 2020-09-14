bf58781a2ce3bc8d45f5520b1869f1cd55f4efb9
NilsSmed <70136449+NilsSmed@users.noreply.github.com>
2020-09-14 13:02:48 +0200

    Update FizzBuzz.py
    Denna borde funka :)

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,19 +1,18 @@
 def main():
     pass
 
-for i in range(1, 100): #problem 1: här printas inte 1 ut, väljer jag range mellan 0 och 100 så printas "fizz" buzz också ut då 5 / 0 blir 0 i pythons värld.
-    if i % 3 == 0:
-        print('fizz')
+for i in range(1,101):
+    x = ""
+    if i % 3 == 0 and (i == 42) is False:
+        x = x + "Fizz"
     if i % 5 == 0:
-        print('buzz')
-    if i % 5 == 0 and i % 3 == 0:
-        print("fizzbuzz")
+        x = x + "Buzz"
+    if x == "":
+        x = i
     if i == 42:
         print("Answer to the Ultimate Question of Life, the Universe, and Everything")
-    else:
-        print(i+1) #problem 2: här printas siffran 42 ut som ska vara ersatt av "answer to the...." jobbar på att göra något form av undantag då 42/3=14 så printas dessutom "fizz" ut  
-
+    elif (i == 42) is False:
+        print (x)
 
 if __name__ == "__main__":
     main()
- 
